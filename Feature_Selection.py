6d0292b7250ae303123f104b50551adb5273556a
arisdour <aris.dour@gmail.com>
2022-04-23 22:09:53 +0300

    Choose Time series (Cases Tests Deaths etc.)

--- Feature_Selection.py
+++ Feature_Selection.py
@@ -7,6 +7,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+from sklearn.feature_selection import mutual_info_regression
 
 
 import tensorflow
@@ -25,66 +26,54 @@ from sklearn.feature_selection import f_regression
     
 import itertools
 from itertools  import product
-from itertools  import combinations 
-
-def readdata(location):
-    
-    data=pd.read_csv(location)
-    Greece= data[data.location =='Greece'].reset_index(drop='True')
-    Greece = Greece.dropna(how='all', axis=1)
-    Greece_total = Greece.iloc[7:498, 3:40].reset_index(drop='True')
-    titles =Greece_total.columns
-    return Greece_total , titles
-loc="owid-covid-data.csv"
+from itertools  import combinations
 
 
-seq_size = 3
-epochs = 60
-times = 10
-Κ= 1
-nodes=0
+def FeatureSelection(df, K):
+    # fs = SelectKBest(score_func=f_regression, k=K)            # Use F regression
+    fs = SelectKBest(score_func=mutual_info_regression, k=K)  # use mutual info
+    df = df.dropna()
+    y = df['total_cases']  # Set Total cases as a Target
+    dates = df['date']
+    X = df.drop(columns=['date', 'total_cases'])  # Remove Total cases
+    X_selected = fs.fit_transform(X, y)
+
+    # X_selected=pd.concat([X_selected, y] , axis=1)
+
+    selected_col = fs.get_support(indices=True)
+    Res = X.iloc[:, selected_col]
+
+    Res = pd.concat([y, Res], axis=1)
+    Final = pd.concat([dates, Res], axis=1)
+    Final = Final.dropna()
+    return Final.columns
+
 
 ##### Data  Creation #####
-Greece_total , titles =readdata(loc)
-Greece_total['new_deaths_smoothed']= Greece_total['new_deaths'].rolling(window=7).mean()
-Greece_total['new_deaths_smoothed']= Greece_total['new_deaths'].rolling(window=7).mean()
-Greece_total['new_deaths_smoothed_per_million']= Greece_total['new_deaths_smoothed']*0.096
-Greece_total['new_deaths_smoothed_per_million']= Greece_total['new_deaths_smoothed']*0.096
+# Greece_total , titles =readdata(loc)
+Greece_total=pd.read_csv(r"owid_dataset_fixed.csv")
+# Remove  ICU *& Hospital Data from original Dataset
+titles = Greece_total.columns
+titles.str.contains('adm')
+admtitles = titles[titles.str.contains('adm')].to_list()
+Greece_total = Greece_total.drop(admtitles, axis=1)
 
 
 
-first_n_column  = Greece_total.iloc[:369 , :14]
-second_n_column = Greece_total.iloc[:369 , 22:26]
-
-
-first_n_column=pd.concat([first_n_column, second_n_column], axis=1)
-first_n_column['stringency_index'] = Greece_total['stringency_index']
-first_n_column = first_n_column.reindex(sorted(first_n_column.columns), axis=1)
-first_n_column = first_n_column.dropna()
-second_n_column= second_n_column.dropna()
 
 
 
-fs = SelectKBest(score_func=f_regression, k=10)
+cases = titles[titles.str.contains('cases')].to_list()
 
-y=first_n_column['total_cases'] # Set Total Cases as a Target
-dates = Greece_total['date']
-X=first_n_column.drop( columns=[ 'date' , 'total_cases']) # Remove Total Cases 
+deaths = titles[titles.str.contains('deaths')].to_list()
+
+tests = titles[titles.str.contains('tests')].to_list()
+
+date = titles[titles.str.contains('date')].to_list()
+Feature = cases+deaths+tests+date
+Greece_total=Greece_total[Feature]
 
 
-# y=df['total_cases'] # Set Total Cases as a Target
-# dates = df['date']
-# X=df.drop( columns=[ 'date' , 'total_cases','tests_units']) # Remove Total Cases 
-X_selected =fs.fit_transform(X, y)
-
-# X_selected=pd.concat([X_selected, y] , axis=1)
-
-selected_col = fs.get_support(indices = True)
-Res = X.iloc[:, selected_col]
-
-Res=pd.concat([y,Res ] , axis=1)
-Final=pd.concat([dates,Res ] , axis=1)
-Final=Final.dropna()
-Final2=Final.columns
-    
+results =FeatureSelection(Greece_total, 1)
+Greece_total=Greece_total[results]
 
