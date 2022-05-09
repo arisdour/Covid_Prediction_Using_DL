c871425d97e087ea2aa8c40d3d60488dfec55a48
arisdour <aris.dour@gmail.com>
2022-05-09 14:16:57 +0300

    Autoarima

--- ARIMA_Greece.py
+++ ARIMA_Greece.py
@@ -5,7 +5,7 @@ from matplotlib.pyplot import figure
 
 
 import math
-import winsound
+
 import tensorflow
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM
@@ -37,11 +37,11 @@ def mean_absolute_percentage_error(y_true, y_pred):
 
 
 def split_data(data, sequence):
-    train_set = data[:355].reset_index(drop=True)
-    validation_set = data[355 - sequence:369].reset_index(drop=True)
-    test_set = data[369 - sequence:].reset_index(drop=True)
+    train_set = data[:len(data)-90]
+    # validation_set = data[355 - sequence:369]
+    test_set = data[-90:]
 
-    return train_set, validation_set, test_set
+    return train_set, test_set
 
 
 def timeseries_gen(seq_size, n_features, train, val, test):
@@ -73,64 +73,103 @@ def plotprediction(ypredict , col,name="" , pname="" , predtype=''):
     plt.savefig("Plots\pred" + name +"_"+ predtype+ ".jpeg"  )
     plt.show()
 
-loc="owid_dataset_fixed.csv"
-Greece_total = pd.read_csv(loc)
-Greece_total['date'] = pd.to_datetime(Greece_total['date']) # convert date column to DateTime
-Greece_total.set_index('date', inplace=True)
-
-analysis = Greece_total[['total_cases']].copy()
-decompose_result_mult = seasonal_decompose(analysis, model="multiplicative")
-
-trend = decompose_result_mult.trend
-seasonal = decompose_result_mult.seasonal
-residual = decompose_result_mult.resid
-
-figure(figsize=(14, 10))
-decompose_result_mult.plot()
-plt.show()
-
-
-########################################################################################################################
-
-plt.rcParams.update({'figure.figsize':(18,10)})
-from numpy import log
-
-
-analysis = analysis.reset_index(drop=[True])
-result = adfuller(analysis.total_cases.dropna())
-print('ADF Statistic: %f' % result[0])
-print('p-value: %f' % result[1])
-
-# Original Series
-fig, axes = plt.subplots(4, 1, sharex=True)
+# loc="owid_dataset_fixed.csv"
+# Greece_total = pd.read_csv(loc)
+# Greece_total['date'] = pd.to_datetime(Greece_total['date']) # convert date column to DateTime
+# Greece_total.set_index('date', inplace=True)
 #
-# axes[0,0].plot(analysis.total_cases);
-# axes[0, 0].set_title('Original Series')
-# plot_acf(analysis.total_cases, ax=axes[0, 1])
-plot_acf(analysis.total_cases, ax=axes[0])
-
-# 1st Differencing
-# axes[1, 0].plot(analysis.total_cases.diff());
-# axes[1, 0].set_title('1st Order Differencing')
-# plot_acf(analysis.total_cases.diff().dropna(), ax=axes[1, 1])
-plot_acf(analysis.total_cases.diff().dropna(), ax=axes[1])
-
-# 2nd Differencing
-# axes[2, 0].plot(analysis.total_cases.diff().diff());
-# axes[2, 0].set_title('2nd Order Differencing')
-# plot_acf(analysis.total_cases.diff().diff().dropna(), ax=axes[2, 1])
-plot_acf(analysis.total_cases.diff().diff().dropna(), ax=axes[2])
-
-
-plot_acf(analysis.total_cases.diff().diff().diff().dropna(), ax=axes[3])
-
-plt.show()
+# analysis = Greece_total[['total_cases']].copy()
+# decompose_result_mult = seasonal_decompose(analysis, model="multiplicative")
+#
+# trend = decompose_result_mult.trend
+# seasonal = decompose_result_mult.seasonal
+# residual = decompose_result_mult.resid
+#
+# figure(figsize=(14, 10))
+# decompose_result_mult.plot()
+# plt.show()
+#
+#
+# ########################################################################################################################
+#
+# plt.rcParams.update({'figure.figsize':(18,10)})
+# from numpy import log
+#
+#
+# analysis = analysis.reset_index(drop=[True])
+# result = adfuller(analysis.total_cases.dropna())
+# print('ADF Statistic: %f' % result[0])
+# print('p-value: %f' % result[1])
+#
+# # Original Series
+# fig, axes = plt.subplots(4, 1, sharex=True)
+# #
+# # axes[0,0].plot(analysis.total_cases);
+# # axes[0, 0].set_title('Original Series')
+# # plot_acf(analysis.total_cases, ax=axes[0, 1])
+# plot_acf(analysis.total_cases, ax=axes[0])
+#
+# # 1st Differencing
+# # axes[1, 0].plot(analysis.total_cases.diff());
+# # axes[1, 0].set_title('1st Order Differencing')
+# # plot_acf(analysis.total_cases.diff().dropna(), ax=axes[1, 1])
+# plot_acf(analysis.total_cases.diff().dropna(), ax=axes[1])
+#
+# # 2nd Differencing
+# # axes[2, 0].plot(analysis.total_cases.diff().diff());
+# # axes[2, 0].set_title('2nd Order Differencing')
+# # plot_acf(analysis.total_cases.diff().diff().dropna(), ax=axes[2, 1])
+# plot_acf(analysis.total_cases.diff().diff().dropna(), ax=axes[2])
+#
+#
+# plot_acf(analysis.total_cases.diff().diff().diff().dropna(), ax=axes[3])
+#
+# plt.show()
 
 # plot_acf(analysis.total_cases.dropna())
-plot_acf(analysis.total_cases.diff().dropna())
-plot_acf(analysis.total_cases.diff().diff().dropna())
-plot_acf(analysis.total_cases.diff().diff().diff().dropna())
-plot_acf(analysis.total_cases.diff().diff().diff().diff().dropna())
-plot_acf(analysis.total_cases.diff().diff().diff().diff().diff().dropna())
-plot_acf(analysis.total_cases.diff().diff().diff().diff().diff().diff().dropna())
+# plot_acf(analysis.total_cases.diff().dropna())
+# plot_acf(analysis.total_cases.diff().diff().dropna())
+# plot_acf(analysis.total_cases.diff().diff().diff().dropna())
+# plot_acf(analysis.total_cases.diff().diff().diff().diff().dropna())
+# plot_acf(analysis.total_cases.diff().diff().diff().diff().diff().dropna())
+# plot_acf(analysis.total_cases.diff().diff().diff().diff().diff().diff().dropna())
+# plt.show()
+
+
+### AUTO ARIMA ###
+from pmdarima.arima import auto_arima
+loc="owid_dataset_fixed.csv"
+Greece_total = pd.read_csv(loc).set_index('date')
+total_cases=Greece_total['total_cases']
+train  , test = split_data(total_cases,0)
+
+plt.plot(train)
+plt.plot(test)
 plt.show()
+
+arima_model =  auto_arima(train,start_p=0, d=1, start_q=0,
+                          max_p=6, max_d=6, max_q=6, start_P=0,
+                          D=1, start_Q=0, max_P=6, max_D=6,
+                          max_Q=6, m=12, seasonal=False,
+                          error_action='warn',trace = True,
+                          supress_warnings=True,stepwise = True,
+                          random_state=20,n_fits = 70 )
+
+arima_model.summary()
+prediction = pd.DataFrame(arima_model.predict(n_periods = 90),index=test.index)
+prediction.columns = ['predicted_cases']
+
+plt.figure(figsize=(8,5))
+plt.plot(train,label="Training")
+plt.plot(test,label="Test")
+plt.plot(prediction,label="Predicted")
+plt.legend(loc = 'lower left')
+plt.show()
+totalpred=pd.DataFrame()
+totalpred=pd.concat([test, prediction], ignore_index=True ,axis=1)
+
+lour1=mean_absolute_percentage_error(test[:7],prediction[:7])
+lour2=mean_absolute_percentage_error(test[:14],prediction[:14])
+lour3=mean_absolute_percentage_error(test[:30],prediction[:30])
+lour4=mean_absolute_percentage_error(test[:60],prediction[:60])
+lour5=mean_absolute_percentage_error(test[:90],prediction[:90])
