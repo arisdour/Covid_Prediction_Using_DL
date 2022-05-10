06af6185c6fe5748468316be638268edb86d4f3f
arisdour <aris.dour@gmail.com>
2022-05-10 23:23:07 +0300

    AUTO ARIMA Attempt

--- ARIMA_Greece.py
+++ ARIMA_Greece.py
@@ -55,8 +55,11 @@ def plotres (trainingset , testset ,pred, pname):
 
 loc="owid_dataset_fixed.csv"
 Greece_total = pd.read_csv(loc,parse_dates=True,index_col="date")
+train,test = split_data(Greece_total,0)
+test = test[['total_cases']].copy()
+train = train[['total_cases']].copy()
 
-analysis = Greece_total[['total_cases']].copy()
+analysis = train[['total_cases']].copy()
 decompose_result_mult = seasonal_decompose(analysis, model="multiplicative")
 
 # trend = decompose_result_mult.trend
@@ -69,92 +72,51 @@ fig.set_size_inches((16, 9))
 fig.tight_layout()
 plt.show()
 
-adttestres = adftest(analysis.diff().diff())
-
-# # Original Series
-# fig, axes = plt.subplots(4, 1, sharex=True)
-# #
-# # axes[0,0].plot(analysis.total_cases);
-# # axes[0, 0].set_title('Original Series')
-# # plot_acf(analysis.total_cases, ax=axes[0, 1])
-# plot_acf(analysis.total_cases, ax=axes[0])
-#
-# # 1st Differencing
-# # axes[1, 0].plot(analysis.total_cases.diff());
-# # axes[1, 0].set_title('1st Order Differencing')
-# # plot_acf(analysis.total_cases.diff().dropna(), ax=axes[1, 1])
-# plot_acf(analysis.total_cases.diff().dropna(), ax=axes[1])
-#
-# # 2nd Differencing
-# # axes[2, 0].plot(analysis.total_cases.diff().diff());
-# # axes[2, 0].set_title('2nd Order Differencing')
-# # plot_acf(analysis.total_cases.diff().diff().dropna(), ax=axes[2, 1])
-# plot_acf(analysis.total_cases.diff().diff().dropna(), ax=axes[2])
-#
-#
-# plot_acf(analysis.total_cases.diff().diff().diff().dropna(), ax=axes[3])
-#
-# plt.show()
-
-# plot_acf(analysis.total_cases.dropna())
-# plot_acf(analysis.total_cases.diff().dropna())
-# plot_acf(analysis.total_cases.diff().diff().dropna())
-# plot_acf(analysis.total_cases.diff().diff().diff().dropna())
-# plot_acf(analysis.total_cases.diff().diff().diff().diff().dropna())
-# plot_acf(analysis.total_cases.diff().diff().diff().diff().diff().dropna())
-# plot_acf(analysis.total_cases.diff().diff().diff().diff().diff().diff().dropna())
-# plt.show()
-
-def mainf():
-
-    ### AUTO ARIMA ###
-    loc="owid_dataset_fixed.csv"
-    Greece_total = pd.read_csv(loc).set_index('date')
-    total_cases=Greece_total['total_cases']
-    predname='cases'
-
-    #Split Data
-    train, test = split_data(total_cases,0)
-
-    plt.plot(train)
-    plt.plot(test)
-    plt.show()
-
-    ### Make Model ###
-    # arima_model =  auto_arima(train,test='adf',start_p=0, d=1, start_q=0,
-    #                           max_p=6, max_d=6, max_q=6, start_P=0,
-    #                           D=1, start_Q=0, max_P=6, max_D=6,
-    #                           max_Q=6, m=12, seasonal=False,
-    #                           error_action='warn',trace = True,
-    #                           supress_warnings=True,stepwise = True,
-    #                           random_state=20,n_fits = 70 )
-
-    arima_model = auto_arima(train, start_p=1, start_q=1,
-                          test='adf',       # use adftest to find optimal 'd'
-                          max_p=6, max_q=6, # maximum p and q
-                          m=12,              # frequency of series
-                              # let model determine 'd'
-                          seasonal=False,   # No Seasonality
-                          start_P=0,
-                          D=0,
-                          trace=True,
-                          error_action='ignore',
-                          suppress_warnings=True,
-                          stepwise=True, )
+adttestres = adftest(analysis.diff().diff()) # Use d =2 for my model
 
 
-    arima_model.summary()
-    arima_model.plot_diagnostics(figsize=(10,8))
-    plt.show()
-    ### Make Prediction ###
-    prediction = pd.DataFrame(arima_model.predict(n_periods = len(test)),index=test.index)
-    prediction.columns = ['predicted_'+ predname]
+############## ACF & PACF Plots ################
+#Calculate p order PACF
+
+fig=plot_pacf(analysis.diff().diff().dropna())
+fig.set_size_inches((16, 9))
+plt.show()
+
+fig=plot_acf(analysis.diff().diff().dropna())
+fig.set_size_inches((16, 9))
+plt.show()
+
+
+arima_model = auto_arima(analysis, start_p=1, start_q=1,
+                      test='adf',       # use adftest to find optimal 'd'
+                      max_p=13, max_q=13, # maximum p and q
+                      m=7,              # frequency of series
+                      d=None,           # let model determine 'd'
+                      seasonal=True,   # Seasonality
+                      start_P=0,
+                      # D=1,
+                      trace=True,
+                      error_action='ignore',
+                      suppress_warnings=True,
+                      stepwise=True)
+
+
+
+
+arima_model.summary()
+arima_model.plot_diagnostics(figsize=(12,8))
+plt.show()
+
+
+### Make Prediction ###
+predname='cases'
+prediction = pd.DataFrame(arima_model.predict(n_periods = len(test)),index=test.index)
+prediction.columns = ['predicted_'+ predname]
 
     ### Results ####
-    plotres(train , test, prediction , predname)
-    totalpred=pd.concat([test, prediction], ignore_index=True ,axis=1)
-    finalresults = final_results(test,prediction)
-
+plotres(train , test, prediction , predname)
+totalpred=pd.concat([test, prediction], ignore_index=True ,axis=1)
+finalresults = final_results(test,prediction)
 
 
 
